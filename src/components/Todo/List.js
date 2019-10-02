90be2d9a0dd1a2431575ad2eb2e68d871b57c260
yp <yalinpala@protonmail.com>
2019-10-02 23:45:57 +0300

    update todo

--- src/components/Todo/List.js
+++ src/components/Todo/List.js
@@ -8,7 +8,9 @@ class List extends Component {
 
     state = {
         todos: this.props.todos,
-        userSession: this.props.userSession
+        userSession: this.props.userSession,
+        todo: '',
+        id: ''
     }
 
     checkedTodo = async (id) => {
@@ -51,9 +53,51 @@ class List extends Component {
             console.log(error);
         }
     }
+    
+    selectedTodo = (id) => {
+        const { todos } = this.state;
+        const todo = todos.find(todo => todo.id === id);
+
+        this.setState({ id: id, todo: todo.todo });
+    }
+
+    updateTodo = async () => {
+        const { todos, todo, id, userSession } = this.state;
+        const options = { encrypt: false };
+        const active = true;
+
+        const uTodo = todos.find(todo => todo.id === id);
+
+        todos.splice(todos.indexOf(uTodo), 1);
+
+        const params = {
+            id,
+            todo,
+            active
+        };
+
+        try {
+            if (todos.length > 0) {
+                await userSession.putFile(TODO_FILENAME, JSON.stringify([...todos, params]), options);
+            } else {
+                await userSession.putFile(TODO_FILENAME, JSON.stringify([params]), options);
+            }
+
+            todos.push(params);
+            this.setState({ todo: '', id: '', todos: todos });
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
+    onChange = (e) => {
+        this.setState({
+            [e.target.name]: e.target.value
+        });
+    }
 
     render() {
-        const { todos } = this.state;
+        const { todos, id, todo } = this.state;
         
         return(
             <Row>
@@ -63,13 +107,22 @@ class List extends Component {
                         {
                             todos.map((t) => 
                                 t.active ?
-                                <InputGroup className="mt-3" key={t.id}>
+                                <InputGroup className="mt-3" key={t.id} >
                                     <InputGroup.Prepend>
                                         <InputGroup.Checkbox onClick={() => this.checkedTodo(t.id)} />
                                     </InputGroup.Prepend>
-                                    <FormControl value={t.todo} disabled />
+                                    <div onClick={() => this.selectedTodo(t.id)}>
+                                        <FormControl 
+                                            value={id === t.id ? todo : t.todo}
+                                            disabled={id === t.id ? false : true}
+                                            name="todo"
+                                            onChange={this.onChange}
+                                        />
+                                    </div>
                                     <InputGroup.Append>
-                                        <Button variant="outline-warning">
+                                        <Button variant="outline-warning" 
+                                            disabled={id === t.id ? false : true} 
+                                            onClick={() => this.updateTodo(t.id)}>
                                             <FontAwesomeIcon icon={faEdit}/>
                                         </Button>
                                         <Button variant="outline-danger" onClick={() => this.deleteTodo(t.id)}>
