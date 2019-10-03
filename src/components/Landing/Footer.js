ab464356528b9177c0684de005f0632890aceb96
yp <yalinpala@protonmail.com>
2019-10-03 22:32:15 +0300

    fab, img, landing

--- src/components/Landing/Footer.js
+++ src/components/Landing/Footer.js
@@ -2,16 +2,20 @@ import React from "react";
 import { Navbar, Nav } from "react-bootstrap";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faBolt } from '@fortawesome/free-solid-svg-icons';
+import { faGithubAlt } from '@fortawesome/free-brands-svg-icons';
 
 const Footer = () => (
     <Navbar bg="dark" variant="dark">
         <Navbar.Brand>Tasko</Navbar.Brand>
         <Nav className="mr-auto">
-            <Nav.Link href="https://blockstack.org/" target="_blank"><FontAwesomeIcon icon={faBolt}/> Blockstack</Nav.Link>
+            <Nav.Link href="https://blockstack.org/" target="_blank">
+                <FontAwesomeIcon icon={faBolt} /> Blockstack
+            </Nav.Link>
         </Nav>
         <Nav>
-            <Nav.Link href="#">Link</Nav.Link>
-            <Nav.Link href="#">Link</Nav.Link>
+            <Nav.Link href="https://github.com/yplog/tasko" target="_blank">
+                <FontAwesomeIcon icon={faGithubAlt} /> Github
+            </Nav.Link>
         </Nav>
     </Navbar>
 );
--- src/components/Landing/HeaderSection.js
+++ src/components/Landing/HeaderSection.js
@@ -1,16 +1,15 @@
 import React from "react";
 import { Jumbotron, Button } from "react-bootstrap";
 
+const jumbotronStyle = {
+    backgroundImage: 'url(/Endless-Constellation.svg)',
+    textAlign: 'center'
+};
+
 const HeaderSection = () => (
-    <Jumbotron fluid>
-        <h1>Hello, world!</h1>
-        <p>
-            This is a simple hero unit, a simple jumbotron-style component for calling
-            extra attention to featured content or information.
-        </p>
-        <p>
-            <Button variant="primary">Learn more</Button>
-        </p>
+    <Jumbotron fluid style={jumbotronStyle}>
+        <h1><u>Welcome to Distributed To Do App</u></h1>
+        <Button className="mt-5" variant="dark" href="#section">Learn more</Button>
     </Jumbotron>
 );
 
